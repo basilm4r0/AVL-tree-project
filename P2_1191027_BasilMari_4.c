aa644af35f39f82ebeb6260e5ae3405719351b4e
Basil Mari <basilm4r0@gmail.com>
2022-05-22 19:13:17 +0300

    Create user interface

--- P2_1191027_BasilMari_4.c
+++ P2_1191027_BasilMari_4.c
@@ -269,9 +269,9 @@ struct Node* LoadData(struct Node* T){
 	char code[16];
 	char department[64];
 	char topics[256];
-	char line[1000];
+	char line[500];
 	in = fopen("courses.txt","r");
-	while(fgets(line,1000,in) != 0){
+	while(fgets(line,499,in) != 0){
 		if(feof(in))
 			break;
 		else {
@@ -296,9 +296,90 @@ void SaveData(FILE *out,struct Node* T){
 }
 
 int main() {
+	int option;
+	char name[64];
+	int credits;
+	char code[16];
+	char department[64];
+	char topics[256];
 	struct Node* T = NULL;
-	T = LoadData(T);
-	InOrder(T);
+	struct Node* temp = NULL;
+
+	printf("\nEnter a number to perform one of the following operations:\n1. Load course list from busses.txt into a tree.\n2. Add new course.\n3. Update the information of a course.\n4. Print courses and their information in lexicographic order.\n5. Print topics associated with a course.\n6. List all courses in a department in lexicographic order.\n7. Delete a course.\n8. Delete all courses that start with a specific letter.\n9. Delete all courses that belong to a department.\n10. Save all information in a file named offered_courses.txt.\n");
+	do {
+		printf("Option: ");		//Printing prompt and reading user input
+		scanf("\n%d", &option);
+		switch (option) {
+
+			case (1):			//Option 1: Load bus information from busses.txt
+				T = LoadData(T);
+				break;
+
+			case (2):			//Option 2: Load passenger information from passengers.txt
+				printf("Enter the information of the course to be added.\nCourse name: ");
+				scanf("%63s", name);
+				printf("Credit hours: ");
+				scanf("%d", &credits);
+				printf("Couse code: ");
+				scanf("%15s", code);
+				printf("Department: ");
+				scanf("%63s", department);
+				printf("Course topics: ");
+				scanf("%255s", topics);
+				Insert(T, name, credits, code, department, topics);
+				break;
+
+			case (3):			//Option 3: Assign loaded passengers to loaded buses and print assignment information
+				printf("Enter the information of the course to be updated.\nCourse name: ");
+				scanf("%63s", name);
+				printf("Credit hours: ");
+				scanf("%d", &credits);
+				printf("Couse code: ");
+				scanf("%15s", code);
+				printf("Department: ");
+				scanf("%63s", department);
+				printf("Course topics: ");
+				scanf("%255s", topics);
+				if (DeleteNode(T, name) == NULL)
+					printf("Course does not exist!\nAdding course to tree...\n");
+				Insert(T, name, credits, code, department, topics);
+				break;
+
+			case (4):			//Option 4: Print information of specified bus along with its passengers
+				if (T != NULL) {
+					printf("\nCourses in tree:\n");
+					InOrder(T);
+				}
+				break;
+
+			case (5):			// Option 5: Print unmatched passengers and their information
+				printf("Course name: ");
+				scanf("%63s", name);
+				if ((temp = Find(name, T)) == NULL)
+					printf("Course does not exit!\n");
+				else
+					printf("Course topics:\n%s\n", temp->topics);
+				break;
+
+			case (6):			// Option 6: Add passenger to passengerList
+				break;
+
+			case (7):			// Option 7: Delete passenger from passengerList
+				break;
+
+			case (8):			// Option 8: Delete bus from busList
+				break;
+
+			case (9):			// Option 9: Exit program
+				printf("Exiting program...\n");
+				break;
+
+			default:			// Default option: print error message and prompt user for valid input
+				printf("Option invalid. Please enter a valid option.\n");
+				break;
+		}
+	} while(option != 9);
+
 	MakeEmpty(T);
 	return 0;
 }
